15142459a821457e1d7a61f07341215535cb0bda
MinaWagdy5 <minawagdy387@gmail.com>
2024-02-18 23:46:53 +0200

    importing csv file for reading and writing

--- learning management/professor.py
+++ learning management/professor.py
@@ -21,21 +21,21 @@ class professor(Person):
             # If the key doesn't exist, raise an error or handle it accordingly
             raise KeyError(f"professor '{key}' not found")
         
-    def append_value(cls, key, value):
-        with open(filename, 'w', newline='') as file:
+    def append_value(cls, key, value, file_name=file_name):
+        with open(file_name, 'a', newline='') as file:
             writer = csv.writer(file)
-            writer.writerows(value)
+            writer.writerow(value)
         cls.representation_professor[key] = value  # if there is name duplicated also append as a list
      # also if the name in not found w append the name as a key and value of his data
 
-    def printing_all_professors(self, file_name):
+    def printing_all_professors(cls, file_name= file_name):
         with open(file_name, 'r') as file:
             reader = csv.reader(file)
             for row in reader:
                 print(row)
 
     def __str__(self):
-        return f"(first_name= {self.first_name},last_name= {self.last_name},age= {self.age},mobile= {self.mobile},email= {self.email},gender= {self.gender},professorID= {self.professorID},position= {self.position},departement= {self.departement},professorCourses= {self.professorCourses})"
+        return f"(first_name= {self.firstName},last_name= {self.lastName},age= {self.age},mobile= {self.mobile},email= {self.email},gender= {self.gender},professorID= {self.professorID},position= {self.position},departement= {self.departement},professorCourses= {self.professorCourses})"
 
     def professorID(self, professorID):
         self.professorID= professorID
