49b877ffddcaf5bb4385fa3aafb11b1cf553c2d9
Ali Zayan <Ali.Esmail03@eng-st.cu.edu.eg>
2024-02-28 01:08:26 +0200

    Add files via upload

---
+++ program.py
@@ -0,0 +1,11 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from learning_management.student import Student
+from learning_management.Person import Person
+
+
+from board5 import Ui_MainWindow as part1
+from board9 import Ui_MainWindow as part2
+
+class Ui_MainWindow(part1, part2):
+    pass
+
